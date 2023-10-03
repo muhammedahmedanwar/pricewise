269b38346c5e9860fb4b6fe5c916f6cd3ac93a69
Jam Jomyyy <mgego0581@gmail.com>
2023-10-04 01:46:42 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter, Space_Grotesk } from 'next/font/google'
+import Navbar from '@/components/Navbar'
+
+const inter = Inter({ subsets: ['latin'] })
+const spaceGrotesk = Space_Grotesk({subsets:['latin'], weight:['300', '400', '500', '600', '700']})
+
+export const metadata: Metadata = {
+  title: 'JimmyZ-Shopping',
+  description: 'Track product prices effortlessy and save money on your online shopping',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+      <main className='max-w-10xl mx-auto'>
+        <Navbar />
+        {children}
+      </main>
+      </body>
+    </html>
+  )
+}
