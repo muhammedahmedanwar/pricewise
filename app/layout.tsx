063eb89e0bded6bf374d2aafd093a3e51fa556f7
Jam Jomyyy <mgego0581@gmail.com>
2023-10-04 02:09:09 +0200

    fix build errorss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,17 @@
+import Navbar from '@/components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter, Space_Grotesk } from 'next/font/google'
-import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
-const spaceGrotesk = Space_Grotesk({subsets:['latin'], weight:['300', '400', '500', '600', '700']})
+const spaceGrotesk = Space_Grotesk({ 
+  subsets: ['latin'], 
+  weight: ['300', '400', '500', '600', '700']
+ })
 
 export const metadata: Metadata = {
-  title: 'JimmyZ-Shopping',
-  description: 'Track product prices effortlessy and save money on your online shopping',
+  title: 'Pricewise',
+  description: 'Track product prices effortlessly and save money on your online shopping.',
 }
 
 export default function RootLayout({
@@ -19,10 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <main className='max-w-10xl mx-auto'>
-        <Navbar />
-        {children}
-      </main>
+        <main className="max-w-10xl mx-auto">
+          <Navbar />
+          {children}
+        </main>
       </body>
     </html>
   )
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -9,28 +9,46 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 
 type Props = {
-    params:{id:string}
+  params: { id: string }
 }
 
-const ProductDetails = async ({params: {id}}:Props) => {
-    const product: Product = await getProductById(id);
-    if(!product) redirect('/')
-    const similarProducts = await getSimilarProducts(id);
+const ProductDetails = async ({ params: { id } }: Props) => {
+  const product: Product = await getProductById(id);
+
+  if(!product) redirect('/')
+
+  const similarProducts = await getSimilarProducts(id);
+
   return (
     <div className="product-container">
-        <div className="flex gap-28 xl:flex-row flex-col">
-            <div className="product-image">
-                <Image src={product.image} alt={product.title} width={580} height={400} className="mx-auto" />
+      <div className="flex gap-28 xl:flex-row flex-col">
+        <div className="product-image">
+          <Image 
+            src={product.image}
+            alt={product.title}
+            width={580}
+            height={400}
+            className="mx-auto"
+          />
+        </div>
+
+        <div className="flex-1 flex flex-col">
+          <div className="flex justify-between items-start gap-5 flex-wrap pb-6">
+            <div className="flex flex-col gap-3">
+              <p className="text-[28px] text-secondary font-semibold">
+                {product.title}
+              </p>
+
+              <Link
+                href={product.url}
+                target="_blank"
+                className="text-base text-black opacity-50"
+              >
+                Visit Product
+              </Link>
             </div>
-            <div className="flex-1 flex flex-col">
-                <div className="flex justify-between items-start gap-5 flex-wrap pb-6">
-                    <div className="flex flex-col gap-3">
-                        <p className="text-[28px] text-secondary font-semibold">{product.title}</p>
-                        <Link href={product.url} target="_blank" className="text-base text-black opacity-50">
-                            Visit Product
-                        </Link>
-                    </div>
-                    <div className="flex items-center gap-3">
+
+            <div className="flex items-center gap-3">
               <div className="product-hearts">
                 <Image 
                   src="/assets/icons/red-heart.svg"
